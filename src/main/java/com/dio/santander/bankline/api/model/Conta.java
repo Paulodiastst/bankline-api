e7dd3ee1585e0c6055654f00f3255a4493159780
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-05-04 19:54:29 -0300

    criamos as classes de modelo da aplicacao

---
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -0,0 +1,21 @@
+package com.dio.santander.bankline.api.model;
+
+public class Conta {
+	private Long numero;
+	private Long saldo;
+	
+	
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public Long getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Long saldo) {
+		this.saldo = saldo;
+	}
+
+}
